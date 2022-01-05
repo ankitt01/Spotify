2c7b117f5e1cd9417a2e5e6a27960b6722d2dbcf
ankitt01 <ankitrawat0102@gmail.com>
2022-01-06 02:23:33 +0530

    completed the async jwt calling handling 3 possible scenarios: the initial sign in where we get our first token: then when we come back to the page we will check if the access token is still valid if not we will refresh the token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,29 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { refreshAccessToken } from "spotify-web-api-node/src/server-methods"
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+
+async function refreshAccessToken(token) {
+    try {
+        spotifyApi.setAccessToken(token.accessToken)
+        spotifyApi.setRefreshToken(token.refreshToken)
+        const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+        return {
+            ...token,
+            accessToken: refreshedToken.access_token,
+            accessTokenExpires: Date.now + refreshedToken.expires_in * 1000, //equals 1 hour
+
+            refreshToken: refreshedToken.refresh_token ? token.refreshToken
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            ...token,
+            error: 'RefreshAccessTokenError',
+        }
+    }
+}
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -7,8 +31,32 @@ export default NextAuth({
         SpotifyProvider({
             clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-            authorization
+            authorization: LOGIN_URL,
         }),
         // ...add more providers here
     ],
+    secret: process.env.JWT_SECRET,
+    pages: {
+        signIn: '/login'
+    },
+    callbacks: {
+        async jwt({ token, account, user }) {
+            //if initial sign in
+            if (account && user) {
+                return {
+                    ...token,
+                    accessToken: account.access_token,
+                    refreshToken: account.refresh_token,
+                    username: account.providerAccountId,
+                    accessTokenExpires: account.expires_at * 1000, //Handling expiring time in milliseconds
+                }
+            }
+            //Return the prev token if access token is not expired
+            if (Date.now() < token.accessTokenExpires) {
+                return token;
+            }
+            //Access token has expired, so we need to refresh it...
+            return await refreshAccessToken(token)
+        }
+    }
 })
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,7 @@
 function Login() {
     return (
         <div>
-            
+            This is login page
         </div>
     )
 }
