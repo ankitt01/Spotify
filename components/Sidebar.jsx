5205cc393e11bf96670d349da3a7d7a0f20b4ccb
ankitt01 <ankitrawat0102@gmail.com>
2022-01-14 10:37:07 +0530

    removed logout from sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon } from "@heroicons/react/outline"
-import {signOut, useSession} from "next-auth/react"
+import {useSession} from "next-auth/react"
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { playlistIdState } from "../atoms/playlistAtom";
@@ -23,18 +23,12 @@ function Sidebar() {
 
     
     return (
-        <div className="text-gray-500 p-5 text-xs lg:text-sm overflow-y-scroll scrollbar-hide h-screen border-r border-gray-900 sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+        <div className="text-gray-400 p-5 text-xs lg:text-sm overflow-y-scroll scrollbar-hide h-screen border-r border-gray-900 sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
             <div className="space-y-4">
                 <button className="flex items-center space-x-2 hover:text-white ">
                     <HomeIcon className="h-5 w-5"/>
                     <p>Home</p>
                 </button>
-
-                <button className="flex items-center space-x-2 hover:text-white " onClick={() => signOut()}>
-                    <p>Logout</p>
-                </button>
-
-
                 <button className="flex items-center space-x-2 hover:text-white ">
                     <SearchIcon className="h-5 w-5"/>
                     <p>Search</p>
